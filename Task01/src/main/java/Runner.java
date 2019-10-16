df180d89542bb8c9cbc0b09c88f84d7c3e3dc42d
EugeneZakhno <zohan1234567@gmail.com>
2019-10-16 23:05:24 +0200

    First commit for Task01

---
+++ Task01/src/main/java/Runner.java
@@ -0,0 +1,9 @@
+
+import user.BooksAccount;
+
+public class Runner {
+    public static void main(String[] args) {
+        BooksAccount booksAccount=new BooksAccount();
+        booksAccount.menu();
+    }
+}
---
+++ Task01/src/main/java/user/UserFactory.java
@@ -0,0 +1,20 @@
+package user;
+
+public class UserFactory {
+
+    private static String password="123456789";
+
+    public User getUser(String parole,String login){
+        if(password.equals(parole)){
+            return new Admin(login,parole);
+        }else{
+            return new User(login,parole);
+        }
+    }
+
+    protected static void changePassword(String oldPassword,String newPassword){
+        if(oldPassword.equals(password) && newPassword!=null && !newPassword.isEmpty()){
+            password=newPassword;
+        }
+    }
+}
